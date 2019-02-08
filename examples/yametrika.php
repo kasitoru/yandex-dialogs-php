ccd8da2f505f1c6893fc5c126c37ec9d08da1045
Sergey Avdeev <thesoultaker48@gmail.com>
2019-02-08 15:29:55 +0300

    Пример для Яндекс.Метрики

---
+++ examples/yametrika.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+	PHP библиотека для разработки навыков Яндекс.Алисы
+	Author: Sergey Avdeev <thesoultaker48@gmail.com>
+	URL: https://github.com/thesoultaker48/yandex-dialogs-php
+*/
+
+
+include_once '../yandex-dialogs.class.php';
+
+$alice = new YandexDialog();
+
+// Подключаем Яндекс.Метрику
+$alice->use_yametrika('YOU_COUNTER_ID');
+
+// Все действия выполняем только если получили корректные данные от Алисы
+if($alice->get_request()) {
+
+	// Начало диалога
+	function _new_session($alice) {
+		$alice->add_button('Робот');
+		$alice->add_message('Привет! Ты кто?');
+	}
+	$alice->bind_new_action('_new_session');
+
+	// Ответ пользователя распознан
+	function _robot($token, $alice) {
+		$alice->add_message('Я так и знал!');
+	}
+	$alice->bind_words_action(array('робот'), '_robot');
+
+	// Неизвестная команда
+	function _default($alice) {
+		$alice->add_message('Я вас не понимаю! Вы, наверное, человек?');
+		// Сообщаем Яндекс.Метрике об этом
+		$alice->yametrika_rgoal('unknown_cmd');
+	}
+	$alice->bind_default_action('_default');
+
+	// Отправляем ответ и завершаем работу скрипта
+	$alice->finish(true);
+}
